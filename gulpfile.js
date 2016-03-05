6625f1f3b9f0f6c074a66b4c8039f22fb11ad3a2
Paul Kubala <paulisdaman@gmail.com>
2016-03-05 10:23:46 -0700

    getting gulp sass wired up

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,14 @@
+'use strict';
 var gulp = require('gulp');
+var sass = require('gulp-sass');
 
-gulp.task('build', function() {
+gulp.task('start', function() {
   // place code for your default task here
-  .pipe('npm build');
+  //.pipe('npm start');
+});
+
+gulp.task('sass', function () {
+  return gulp.src('./sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
 });
